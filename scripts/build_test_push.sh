1c2607eaa3af04e4e6e39f257f83844b8f82c3e0
Fabijan Bajo <bajo09@gmail.com>
2019-05-15 09:01:03 +0200

    add gitlab

--- scripts/build_test_push.sh
+++ scripts/build_test_push.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 function tekton_pipeline_clean() {
-  k delete PipelineResource --all && \
-    k delete tasks.tekton.dev --all && \
-    k delete taskruns.tekton.dev --all
+  kubectl delete PipelineResource --all && \
+    kubectl delete tasks.tekton.dev --all && \
+    kubectl delete taskruns.tekton.dev --all
 }
 
 function tekton_pipeline_build() {
-  apply -f tektoncd/notes-git-resource.yaml && \
-    k apply -f tektoncd/notes-image-resource.yaml && \
-    k apply -f tektoncd --recursive && \
-    pod=$(k -n tekton-pipelines get pod -l tekton.dev/taskRun=notes-build-taskrun -o jsonpath='{.items[0].metadata.name}')
+  kubectl apply -f tektoncd/notes-git-resource.yaml && \
+    kubectl apply -f tektoncd/notes-image-resource.yaml && \
+    kubectl apply -f tektoncd --recursive && \
+    pod=$(kubectl -n tekton-pipelines get pod -l tekton.dev/taskRun=notes-build-taskrun -o jsonpath='{.items[0].metadata.name}')
 }
 
 tekton_pipeline_clean
